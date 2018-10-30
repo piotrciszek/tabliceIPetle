19435f7a3f44562493cdd874e4ea235e5ff11fc1
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-10-30 19:20:58 +0100

    Zadanie6 added

---
+++ src/main/java/Zadanie6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Zadanie6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wyraz, program wyświeti go na odwrót");
+        String word = scanner.nextLine();
+        String[] letters = word.split("");
+        String drow = "";
+        for (int i = letters.length-1; i >= 0; i--) {
+            drow += letters[i];
+        }
+        System.out.println("Wyraz po odwroceniu to: " + drow);
+    }
+
+}
