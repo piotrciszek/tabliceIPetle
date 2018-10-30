c6f7cc107a3bb8894981716ae9dd6f8959dcc64d
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-10-30 19:12:36 +0100

    Zadanie5 added

---
+++ src/main/java/Zadanie5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Zadanie5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj liczbę, zostanie obliczona suma jej cyfr");
+        String number = scanner.nextLine();
+        String[] digits = number.split("");
+        int sum = 0;
+
+        for (int i = 0; i < digits.length; i++) {
+            sum += Integer.parseInt(digits[i]);
+
+        }
+        System.out.println("Wynik to: " + sum);
+    }
+
+}
