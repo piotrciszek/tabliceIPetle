31f84336f4688e3d5fca3843eac8adf055a7bc63
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-03 22:09:14 +0100

    Zadanie "statystykiTablicy"

---
+++ src/main/java/zadaniaDodatkowe/StatystykiTablicy.java
@@ -0,0 +1,36 @@
+package zadaniaDodatkowe;
+
+public class StatystykiTablicy {
+    public static void main(String[] args) {
+
+        int[] tablica = new int[]{2,5,1,-3,5,22,21,66,21,54};
+
+        int min = tablica[0];
+
+        for (int i= 1; i<tablica.length; i++) {
+            if (tablica[i] < min) {
+                min = tablica[i];
+            }
+        }
+        System.out.println("Minimalna wartosc tabeli to: " + min);
+
+        int max = tablica[0];
+
+        for (int i=0; i<tablica.length; i++) {
+            if (tablica[i] > max) {
+                max = tablica[i];
+            }
+        }
+        System.out.println("Maksymalna wartosc tabeli to: " + max);
+
+        double sum = 0;
+        for (int z : tablica) {
+            sum += z;
+        }
+        System.out.println("Srednia z tabeli to: " + sum/tablica.length);
+
+        System.out.println("W tabeli znajduje się " + tablica.length + " elementów");
+    }
+
+
+}
