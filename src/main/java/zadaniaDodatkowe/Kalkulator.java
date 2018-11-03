1323d2a5ac194f97e9e51b3f55fc1fe42ac7f57d
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-03 21:44:24 +0100

    Zadanie "LosowaLiczba" + tresc zadania

--- src/main/java/zadaniaDodatkowe/Kalkulator.java
+++ src/main/java/zadaniaDodatkowe/Kalkulator.java
@@ -1,9 +1,9 @@
-package kalkulator;
+package zadaniaDodatkowe;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class Main {
+public class Kalkulator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj rodzaj działania: +,-,*,/");
