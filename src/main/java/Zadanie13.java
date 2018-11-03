27df883b0d82e24f38258ff836749c24766c1561
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-03 13:00:12 +0100

    Zadanie13bcd dodane

--- src/main/java/Zadanie13.java
+++ src/main/java/Zadanie13.java
@@ -6,6 +6,39 @@ public class Zadanie13 {
                 System.out.print("#");
             }
         }
+        System.out.println();
+        System.out.print("------");
+
+        for (int i=5; i>0; i--) {
+            System.out.println();
+            for (int j=0; j<i; j++){
+                System.out.print("#");
+            }
+        }
+        System.out.println();
+        System.out.print("------");
+
+        for(int i = 5; i > 0; i-- ){
+            System.out.println();
+            for(int k = 5; k > i ; k--){
+                System.out.print(" ");
+            }
+            for( int j = 0; j < i; j++){
+                System.out.print("#");
+            }
+        }
+        System.out.println();
+        System.out.print("------");
+
+        for (int i=0; i<5; i++){
+            System.out.println();
+            for (int k =5; k > i; k--){
+                System.out.print(" ");
+            }
+            for (int j=0; j<i; j++){
+                System.out.print("#");
+            }
+        }
     }
 
 
