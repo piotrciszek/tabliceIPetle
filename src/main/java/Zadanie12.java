78498181e088106f686caebdf077bf1c28cc8cd9
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-03 12:42:37 +0100

    Zadanie12 do/while added ograniczenie corrected

--- src/main/java/Zadanie12.java
+++ src/main/java/Zadanie12.java
@@ -3,24 +3,21 @@ import java.util.Scanner;
 public class Zadanie12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println("Podaj liczbę od 1 do 20, program narysuje trójką z tych liczb");
-
-
-        int liczba = scanner.nextInt();
-
-        if (liczba > 20) {
-            System.out.println("Podaj jeszcze raz liczbe, z zakresu 1-20");
+        int liczba =0;
+        do {
+            System.out.println("Podaj liczbę od 1 do 20, program narysuje trójką z tych liczb");
             liczba = scanner.nextInt();
-        } else {
-
-            for (int i = 1; i <= liczba; i++) {
-                System.out.println();
-                for (int j = 1; j <= i; j++) {
-                    System.out.print(j);
+            if (liczba<=20) {
+                for (int i = 1; i <= liczba; i++) {
+                    System.out.println();
+                    for (int j = 1; j <= i; j++) {
+                        System.out.print(j);
+                    }
                 }
             }
-        }
+
+        } while (liczba > 20) ;
+
+
     }
 }
