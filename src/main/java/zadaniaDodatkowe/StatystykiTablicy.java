adb6df6dac3bd92e2fbadef7ddd88c7ec5755250
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-04 18:25:45 +0100

    Zadanie "statystykiTablicy" dodana metoda 'srednia'

--- src/main/java/zadaniaDodatkowe/StatystykiTablicy.java
+++ src/main/java/zadaniaDodatkowe/StatystykiTablicy.java
@@ -3,7 +3,15 @@ package zadaniaDodatkowe;
 public class StatystykiTablicy {
     public static void main(String[] args) {
 
+
+
         int[] tablica = new int[]{2,5,1,-3,5,22,21,66,21,54};
+
+        System.out.println("---");
+        System.out.println(srednia(tablica));
+        System.out.println("-----");
+        srednia(tablica);
+        System.out.println("--------");
 
         int min = tablica[0];
 
@@ -31,6 +39,12 @@ public class StatystykiTablicy {
 
         System.out.println("W tabeli znajduje się " + tablica.length + " elementów");
     }
-
+    private static double srednia(int[] tablica){
+        double sum = 0;
+        for (int z : tablica) {
+            sum+= z;
+        }
+        return sum/tablica.length;
+    }
 
 }
