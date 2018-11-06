cd343c08bfba9e4869a5398b79b5bc6282bb7926
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-06 20:07:18 +0100

    Zadanie "Wisielec" do poprawy Zadanie "Szyfrolamacz" dodane wraz z trescia

--- src/main/java/zadaniaDodatkowe/Wisielec.java
+++ src/main/java/zadaniaDodatkowe/Wisielec.java
@@ -13,40 +13,53 @@ public class Wisielec {
         char[] tablicaHaslo = haslo.toCharArray();
 
         char[] tablicaKopia = new char[tablicaHaslo.length];
+
         for (int i =0; i<tablicaKopia.length; i++){
             tablicaKopia[i] = '*';
         }
         for (char f : tablicaKopia) {
             System.out.print(f);
         }
+
+        System.out.println();
         while (iloscSzans>0){
             System.out.println("Podaj literę");
-            char litera = scanner.nextLine().charAt(0);
+            String literaS = scanner.nextLine();
+            char listeraSS = literaS.charAt(0);
 
-            if (czyZawiera(haslo, litera)) {
+            if (czyZawiera(tablicaHaslo, listeraSS)) {
                 System.out.println("Brawo trafiles");
 
-                }
+
+                System.out.println(wyswietlHaslo(tablicaHaslo, tablicaKopia, listeraSS));
+                System.out.println();
+            }
+
              else {
                 System.out.println("Nie ma takiej litery, tracisz szanse");
                 iloscSzans--;
             }
         }
 
-
-
-
-
-
-
     }
-    private static boolean czyZawiera(String tekst, char znak){
-        for (int i=0; i<tekst.length(); i++) {
-            if (tekst.charAt(i) == znak){
+    private static boolean czyZawiera(char[] hasloTablica, char znak){
+        for (int i=0; i<hasloTablica.length; i++) {
+            if (hasloTablica[i] == znak){
                 return true;
             }
         }
         return false;
+    }
+    private static String wyswietlHaslo(char[] hasloTablica, char[] kopia, char znak){
+        int index = 0;
+        for (int i=0; i<hasloTablica.length; i++) {
+            if (hasloTablica[i] == znak){
+
+
+            }
+            kopia[index] = znak;
+        }
+        return kopia.toString();
     }
 
 }
